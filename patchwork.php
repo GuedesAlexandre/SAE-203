7e3c21194aab3b7da2fe0e393e82483e6f2b98aa
AGuedes <115166939+GuedesAlexandre@users.noreply.github.com>
2023-04-19 09:01:42 +0200

    algo pour récupérer le numéro étudiant

--- patchwork.php
+++ patchwork.php
@@ -36,6 +36,24 @@ if (mysqli_num_rows($resultat) == 1) {
     echo "Adresse email incorrecte.";
 }
 
+//pour envoie de mail
+$to = $_POST['email'];
+$subject = "Email Subject";
+
+$message = 'Dear '.$_POST['name'].',<br>';
+$message .= "We welcome you to be part of family<br><br>";
+$message .= "Regards,<br>";
+
+// Always set content-type when sending HTML email
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+// More headers
+$headers .= 'From: <enquiry@example.com>' . "\r\n";
+$headers .= 'Cc: myboss@example.com' . "\r\n";
+
+mail($to,$subject,$message,$headers);
+
 // Fermeture de la connexion à la base de données
 mysqli_close($conn);
     ?>
