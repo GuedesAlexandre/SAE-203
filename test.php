bfe0df85a8b29a2eec983e530eb6ec9ceeab1af9
AGuedes <115166939+GuedesAlexandre@users.noreply.github.com>
2023-04-22 15:02:16 +0200

    statut ok

--- test.php
+++ test.php
@@ -133,7 +133,7 @@ $sql = "SELECT IDE,Prenom FROM utilisateurs WHERE email='$emailuti'";
         }
 
 
-    }else{
+ }else{
         echo "problème";
     }
 
@@ -145,23 +145,41 @@ echo "<form action='test.php' method='post'>
 <input type='submit' name='verif'>
 </form>";
 if(isset($_POST["verif"])){
-    $sql_verif_materiels = "SELECT * FROM Emprunt WHERE statut = 0;";
-    if(mysqli_query($conn, $sql_verif_materiels)){
-        $Emprows = mysqli_fetch_assoc($sql_verif_materiels);
-        $IDEencours = $Emprows["IDE"];
-        $IDencours = $Emprows["ID"];
-        $accept = 1;
-$sqlaccept =  "UPDATE Emprunt SET statut = '$accept' WHERE IDE  = '$IDEencours' AND ID = '$IDencours'  ;";
-        if(mysqli_query($conn, $sqlaccept)){
-            echo "materiels réservé";
-            $sqlgestquant = "UPDATE Materiels SET Quantité = Quantité - 1 WHERE IDE ='$IDEencours' AND ID = '$IDencours';";
-            if(mysqli_query($conn, $sqlgestquant)){
-                echo"quantité-1";
+    $sqlrecup = "SELECT * FROM Emprunt WHERE statut = 0" ;
+    $sql_recupid_from_emprunt = "SELECT ID FROM Emprunt WHERE statut = 0";
+    $sql_recupIDE_from_emprunt = "SELECT IDE FROM Emprunt WHERE statut =0";
+    $recupall = mysqli_query($conn, $sqlrecup);
+    $recupid = mysqli_query($conn, $sql_recupid_from_emprunt);
+    $recupIDE = mysqli_query($conn, $sql_recupIDE_from_emprunt);
+    if(mysqli_num_rows($recupall) == 1 && mysqli_num_rows($recupid) && mysqli_num_rows($recupIDE)){
+        $IDE = mysqli_fetch_assoc($recupIDE);
+        $ID = mysqli_fetch_assoc($recupid);
+        $IDEINRUN = $IDE["IDE"];
+        $IDINRUN = $ID["ID"];
+
+
+
+        $sqlupdatestatut = " UPDATE Emprunt SET statut = statut + 1  WHERE statut = 0 ;";
+        if(mysqli_query($conn, $sqlupdatestatut)){
+            echo "reservation agree";
+                $sqlquant= " SELECT Quantité FROM Materiels WHERE ID = '$IDEINRUN';";
+                
+                }
+
             }
-        }
-}
+
+
+
+
 
 }
+
+
+
+
+
+
+
 
 // Fermer la connexion
 
