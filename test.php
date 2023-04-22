6c498d66e0578cceda4fede52e1709d561877745
AGuedes <115166939+GuedesAlexandre@users.noreply.github.com>
2023-04-22 16:31:53 +0200

    tab

--- test.php
+++ test.php
@@ -2,7 +2,7 @@
 <head>
     <title>Test</title>
     <link rel="stylesheet" href="css/reservation.css">
-  
+
 </head>
 <body>
  <header>
@@ -16,7 +16,7 @@
             </div>
         </nav>
     </header>
-    
+
 <?php
 session_start();
 
@@ -38,18 +38,18 @@ $resultat = mysqli_query($conn, $sql2);
 // Vérifier si la requête a retourné des résultats
 if (mysqli_num_rows($resultat) > 0) {
     // Créer un menu déroulant (select) avec les noms de matériels
-    
-  echo '
+
+    echo '
    <main>
    <div class="containform">
             <form class="form" action="test.php" method="post">
                 <h2>RESERVATION OF EQUIPMENTS</h2>
   <select name="equipement">';
-  while ($row = mysqli_fetch_assoc($resultat)) {
-      echo '<option value="' . $row["nom"] . '">' . $row["nom"] . '</option>';
-  }
-  echo '</select>';
-  echo ' <div class="form-date">
+    while ($row = mysqli_fetch_assoc($resultat)) {
+        echo '<option value="' . $row["nom"] . '">' . $row["nom"] . "</option>";
+    }
+    echo "</select>";
+    echo ' <div class="form-date">
                     <input type="datetime-local" name="start" placeholder="Start date" required>
                     <input type="datetime-local" name="end" placeholder="End date" required>
                 </div>
@@ -75,85 +75,75 @@ if (mysqli_num_rows($resultat) > 0) {
            </div>
         </div>
     </main>';
-  
 } else {
     echo "Aucun résultat trouvé";
 }
-if(!empty($_POST["start"]) && !empty($_POST["end"]) && !empty($_POST["equipement"]) && !empty($_POST["commentaire"])){
+if (
+    !empty($_POST["start"]) &&
+    !empty($_POST["end"]) &&
+    !empty($_POST["equipement"]) &&
+    !empty($_POST["commentaire"])
+) {
     $emailuti = $_SESSION["emailuti"];
 
     $conn = mysqli_connect("localhost", "root", "root", "Matos");
     if (!$conn) {
         die("La connexion a échoué : " . mysqli_connect_error());
     }
-$sql = "SELECT IDE,Prenom FROM utilisateurs WHERE email='$emailuti'";
+    $sql = "SELECT IDE,Prenom FROM utilisateurs WHERE email='$emailuti'";
     $resultat = mysqli_query($conn, $sql);
 
     if (mysqli_num_rows($resultat) == 1) {
         // L'utilisateur a été trouvé dans la base de données
-    $IDEutilisateur = mysqli_fetch_assoc($resultat);
-    $IDE = $IDEutilisateur["IDE"];
-    //récupération de l'entrée dans session
-    $_SESSION["materiels"] = $_POST["equipement"];
-    
-    $_SESSION["debut"] = $_POST["start"];
-    $_SESSION["fin"] = $_POST["start"];
-    $_SESSION["commentaire"] = $_POST["commentaire"];
-    //placement dans des variables
-    $mat = $_SESSION["materiels"];
-    $debut = $_SESSION["debut"];
-    $fin = $_SESSION["fin"];
-    $comment= $_SESSION["commentaire"];
-    //récupération de la date local
-    $date = date("Y-m-d H:i:s");
+        $IDEutilisateur = mysqli_fetch_assoc($resultat);
+        $IDE = $IDEutilisateur["IDE"];
+        //récupération de l'entrée dans session
+        $_SESSION["materiels"] = $_POST["equipement"];
 
-    //requête pour récupérer l'id et la quantité
-    $sqlMATID ="SELECT ID FROM Materiels WHERE nom ='$mat'";
-    $sqlMATQ ="SELECT Quantité FROM Materiels WHERE nom ='$mat'";
-    $resultMATQ = mysqli_query($conn, $sqlMATQ);
-    $resultMATID = mysqli_query($conn, $sqlMATID);
-    if(mysqli_num_rows($resultMATQ) == 1 && mysqli_num_rows($resultMATID) ==1){
-        $IDMAT = mysqli_fetch_assoc($resultMATID);
-        $quantMAT = mysqli_fetch_assoc($resultMATQ);
-        $idmateriels = $IDMAT["ID"];
-        $quantité = $quantMAT["Quantité"];
-    }
+        $_SESSION["debut"] = $_POST["start"];
+        $_SESSION["fin"] = $_POST["start"];
+        $_SESSION["commentaire"] = $_POST["commentaire"];
+        //placement dans des variables
+        $mat = $_SESSION["materiels"];
+        $debut = $_SESSION["debut"];
+        $fin = $_SESSION["fin"];
+        $comment = $_SESSION["commentaire"];
+        //récupération de la date local
+        $date = date("Y-m-d H:i:s");
 
-    if($quantMAT > 0){
-        $sqlReserv = "INSERT INTO Emprunt (IDE,ID,DateDebut,DateFin)
-        VALUES ('$IDE', '$idmateriels', '$debut', '$fin');";
-
-        if( mysqli_query($conn, $sqlReserv)){
-            echo "votre demande à été envoyé";
-
-
-
-        }else{
-         echo   'marche pas';
+        //requête pour récupérer l'id et la quantité
+        $sqlMATID = "SELECT ID FROM Materiels WHERE nom ='$mat'";
+        $sqlMATQ = "SELECT Quantité FROM Materiels WHERE nom ='$mat'";
+        $resultMATQ = mysqli_query($conn, $sqlMATQ);
+        $resultMATID = mysqli_query($conn, $sqlMATID);
+        if (
+            mysqli_num_rows($resultMATQ) == 1 &&
+            mysqli_num_rows($resultMATID) == 1
+        ) {
+            $IDMAT = mysqli_fetch_assoc($resultMATID);
+            $quantMAT = mysqli_fetch_assoc($resultMATQ);
+            $idmateriels = $IDMAT["ID"];
+            $quantité = $quantMAT["Quantité"];
         }
 
+        if ($quantMAT > 0) {
+            $sqlReserv = "INSERT INTO Emprunt (IDE,ID,DateDebut,DateFin)
+        VALUES ('$IDE', '$idmateriels', '$debut', '$fin');";
 
- }else{
-        echo "produits indisponible";
+            if (mysqli_query($conn, $sqlReserv)) {
+                echo "votre demande à été envoyé";
+            } else {
+                echo "marche pas";
+            }
+        } else {
+            echo "produits indisponible";
+        }
     }
-
-
-    }
-
 }
-
-
-
-
-
-
-
-
 
 // Fermer la connexion
 
 mysqli_close($conn);
-
 ?>
 </body>
 </html>
