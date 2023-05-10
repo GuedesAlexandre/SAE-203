39c6808007b939051bcd8d4f8c82f6ac709441bd
AGuedes <115166939+GuedesAlexandre@users.noreply.github.com>
2023-05-10 14:38:35 +0200

    new

---
+++ materielist.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+    <title>Matériels</title>
+    <link rel="stylesheet" href="css/Materiels.css">
+</head>
+<body>
+
+<div class="box">
+    <form name="search">
+        <input type="text" class="input" name="txt" onmouseout="this.value = ''; this.blur();">
+    </form>
+    <i class="fas fa-search"></i>
+
+</div>
+<a href="https://www.youtube.com/c/ShortCode" target="_blank" id="ytb">
+<i class="fab fa-youtube"> </i>
+</a>
+    <div class="container">
+        <div class="box">
+        <h2></h2>
+        <p></p>
+        </div>
+        
+    
+    </div>
+</body>
+</html>
