5c7e4b0e9d35a1589c93ff526e1c0bafb013e61d
AGuedes <115166939+GuedesAlexandre@users.noreply.github.com>
2023-05-12 08:48:52 +0200

    Liste de Matériels

--- materielist.php
+++ materielist.php
@@ -2,21 +2,21 @@
 <head>
     <title>Matériels</title>
     <link rel="stylesheet" href="css/Materiels.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet"/>
 </head>
 <body>
+<header>
 
-<div class="box">
-    <form name="search">
-        <input type="text" class="input" name="txt" onmouseout="this.value = ''; this.blur();">
-    </form>
-    <i class="fas fa-search"></i>
+</header>
+<input type="search" placeholder="search">
 
-</div>
 
     <div class="container">
         <div class="box">
-        <h2></h2>
-        <p></p>
+        <h2>Matériels</h2>
+        <p>Coucou ceci est la description</p>
         </div>
         
     
