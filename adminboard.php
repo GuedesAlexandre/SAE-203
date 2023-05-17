7d85b5888b6a1f662f1f082c09822d5d9b764045
AGuedes <115166939+GuedesAlexandre@users.noreply.github.com>
2023-05-17 11:21:57 +0200

    cc

--- adminboard.php
+++ adminboard.php
@@ -33,6 +33,10 @@
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
     <script src="js/reservation.js"></script>
+    <main>
+    <div class="banner">
+    <img src="img/casque.png" class="casque">
+    </div>
 
     </main>
 
--- reservation.php
+++ reservation.php
@@ -32,6 +32,7 @@
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
     <script src="js/reservation.js"></script>
+    <main>
 
 <?php
 session_start();
