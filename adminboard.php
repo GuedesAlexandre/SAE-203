d49243f5fe0c80b516544ec9ceb6e4fe28d31b0e
AGuedes <115166939+GuedesAlexandre@users.noreply.github.com>
2023-05-17 11:17:18 +0200

    ajout du board admin

---
+++ adminboard.php
@@ -0,0 +1,52 @@
+<html>
+<head>
+    <title>Test</title>
+    <link rel="stylesheet" href="css/reservation.css">
+
+</head>
+<body>
+    <header>
+        <nav class="navbar">
+            <a href='#' class="logo">MATOS</a>
+            <div class="navlinks">
+                <div class="relat">
+                    <a class="elt elt-hov" href="#">Accueil</a>
+                </div>
+                <div class="relat">
+                    <a class="elt elt-hov" href="#">Réservations</a>
+                </div>
+                <div class="relat">
+                    <a class="elt elt-hov" href="#">Demandes</a>
+                </div>
+                <div class="ligne"></div>
+                <ion-icon name="person" class="icon" onclick="taille()"></ion-icon>
+            </div>
+        </nav>
+    </header>
+    <div class="deco">
+        <div class="box">
+            <ion-icon name="log-out-outline"></ion-icon>
+            <a href="déconnexion.php">Déconnexion</a>
+        </div>
+    </div>
+
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    <script src="js/reservation.js"></script>
+
+    </main>
+
+
+<footer>
+    <div class="foot">
+        <ul class="namelist">
+            <li>Arnaud</li>
+            <li>Alexandre</li>
+            <li>Steven</li>
+        </ul>
+        <span>©MATOS | 2023 | Mentions Légales</span>
+    </div>
+</footer>
+
+</body>
+</html>
